86d0566d4a53bc18022ff0c46af80cd3c6502e67
Changiin Lee <changiinlee@Changiinui-MacBookPro.local>
2024-05-19 21:01:06 +0900

    init

--- src/server.py
+++ src/server.py
@@ -9,7 +9,7 @@ def create_app() -> FastAPI:
     
     @app.get("/test")
     def test():
-        return {"result": "oauthquake-v3"}
+        return {"result": "oauthquake-v4"}
 
     return app
     
